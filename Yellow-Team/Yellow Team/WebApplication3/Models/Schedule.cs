bae2663f9ea14f7dc122735d48d0f4828c464be4
GuptaNikhil88 <nikhil.damoh@gmail.com>
2017-06-19 23:14:41 -0400

    workspace

---
+++ Yellow-Team/Yellow Team/WebApplication3/Models/Schedule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        [Display(Name ="Duration")]
+        public int Length { get; set; }
+        //[Display(Name ="Parent Present")]
+        //public bool Parent { get; set; }
+        //public string ScheduledCheckIn { get; set; }
+        //public string ScheduledCheckout { get; set; }
+        //public string EstimatedCheckin { get; set; }
+        //public string EstimatedCheckout { get; set; }
+        [Display(Name = "In")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0:hh:mm tt}")]
+        public string CheckIn { get; set; }
+        [Display(Name = "Out")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:hh:mm tt}")]
+        public string CheckOut { get; set; }
+        public int Priority { get; set; }
+        public string created { get; set; }
+        public int completed { get; set; }
+        public bool CheckedIn { get; set; }
+        //public bool finalized { get; set; }
+        //public int late { get; set; }
+        public virtual int RoomId { get; set; }
+        public virtual Rooms room { get; set; }
+
+    }
+}
